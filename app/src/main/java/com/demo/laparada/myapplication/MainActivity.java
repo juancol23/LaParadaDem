ec7557c438e984ad0ab4ffaa726ca0ca40f95982
Valdemar <juan.colichon@vasslatam.com>
2018-10-22 01:11:35 -0500

    Sprint-21 -fix menu

--- app/src/main/java/com/demo/laparada/myapplication/MainActivity.java
+++ app/src/main/java/com/demo/laparada/myapplication/MainActivity.java
@@ -32,15 +32,15 @@ public class MainActivity extends AppCompatActivity
         menu_inicio();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+  
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
